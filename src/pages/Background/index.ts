4792e8207797c2e9a6c8adda0bc3bfdad52d6b4f
ban vuong <vuongdieuban@gmail.com>
2020-12-25 17:08:34 -0600

    move auth to background ts

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,5 +1,10 @@
 import '../../assets/img/icon-34.png';
 import '../../assets/img/icon-128.png';
+import { ApiConfig, OperationMode } from 'api-lib/dist/config';
+import { AuthenticationService } from 'api-lib/dist/services/';
+
+const env = process.env.NODE_ENV;
+ApiConfig.setOperationMode(env === 'development' ? OperationMode.DEV : OperationMode.PROD);
 
 import { Person } from '../types';
 
@@ -12,3 +17,8 @@ const name = createPerson({ name: 'Ban' });
 console.log('Hello', name);
 
 console.log('This is the background page.');
+
+const authService = new AuthenticationService();
+authService.renewToken().then(data => {
+  console.log('Renew Token', data);
+});
--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
 import { Person } from '../types';
 import './Popup.css';
-import { ApiConfig, OperationMode } from 'api-lib/dist/config';
-import { PlatformService } from 'api-lib/dist/services/platform.service';
-
-const env = process.env.NODE_ENV;
-ApiConfig.setOperationMode(env === 'development' ? OperationMode.DEV : OperationMode.PROD);
 
 const createPerson = (p: Person) => {
   return p.name.toUpperCase();
 };
-
-const service = new PlatformService();
-service.findAll().then(data => {
-  console.log('Platform', data);
-});
 
 function App() {
   const person = createPerson({ name: 'Ban' });
